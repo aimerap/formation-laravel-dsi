07bcda053c8660fd74c755ab7c20ef5a1effcc6b
Compaoré Ezéchiel <cezechiel@live.fr>
2021-03-29 14:28:19 +0000

    laravel ui, sweetalert,

--- resources/views/pages/front-office/list-produits.blade.php
+++ resources/views/pages/front-office/list-produits.blade.php
@@ -35,11 +35,21 @@
                                 <td> {{ $produit->pays_source }} </td>
                                 <td>
                                     <div class="d-flex">
-                                        <a href="{{route('delete',$produit->id)}}"  class="btn btn-danger  mr-2">
+                                        <a href="#" onClick="deleteConfirm('{{'produitItem'.$produit->id}}')" class="btn btn-danger  mr-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" style='width:25px'>
+                                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"  />
+                                            </svg>
+                                        </a>
+                                        <form id="{{'produitItem'.$produit->id}}" 
+                                            action="{{route('delete',$produit->id)}}" 
+                                            method="GET" style="display: none;">
+                                            @csrf
+                                        </form>
+                                        {{-- <a href="{{route('delete',$produit->id)}}"  class="btn btn-danger  mr-2">
                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" style='width:25px'>
                                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"  />
                                              </svg>
-                                       </a>
+                                       </a> --}}
                                         <a href="{{route('ajoutercommande',$produit->id)}}"  class="btn btn-primary">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"  style='width:25px'>
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
